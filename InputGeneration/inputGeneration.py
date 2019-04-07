7236dab7686631049d49690ea0ff8d4e2f74549a
Valentin <valentin.vignal@gmail.com>
2019-04-07 19:29:55 +0800

    Change the shape of the inputs generation

--- InputGeneration/inputGeneration.py
+++ InputGeneration/inputGeneration.py
@@ -27,8 +27,8 @@ def getAllInputs(filename):
         waveform = np.array([[[i] for i in left], 
                              [[i] for i in right]])
         # spectrogram - (2, 1025, 431)
-        spectrogram = np.array([librosa.core.stft(left), 
-                                librosa.core.stft(right)])
+        spectrogram = np.abs(np.array([librosa.core.stft(left),
+                                       librosa.core.stft(right)]))
         #  rms - (2, 1, 431)
         rms = np.array([librosa.feature.rms(left), 
                         librosa.feature.rms(right)])
@@ -37,7 +37,7 @@ def getAllInputs(filename):
                         librosa.feature.zero_crossing_rate(right)])
         #  sc - (2, 1, 431)
         sc = np.array([librosa.feature.spectral_centroid(left),
-                        librosa.feature.spectral_centroid(right)])
+                       librosa.feature.spectral_centroid(right)])
         #  sr - (2, 1, 431)
         sr = np.array([librosa.feature.spectral_rolloff(left),
                        librosa.feature.spectral_rolloff(right)])
@@ -47,16 +47,45 @@ def getAllInputs(filename):
         #  mel_spectrogram - (2, 128, 431)
         mel_spectrogram = np.array([librosa.feature.melspectrogram(left), 
                                     librosa.feature.melspectrogram(right)])
-        # getStats - (4, 2, 1)
+        # getStats - (5, 2, 1)
         stats = np.concatenate([getStats(rms), getStats(zcr), 
                                 getStats(sc), getStats(sr), getStats(sfm)])
 
-        data = (waveform, spectrogram, rms, zcr, mel_spectrogram, stats)
+
+        #### Reshape for the neural network #####
+        # Waveform
+        waveform = np.reshape(waveform, (2, 220500))
+
+        # spectrogram
+        # Good
+
+        # Features
+        features = np.concatenate(
+            [
+                np.reshape(rms, (2, 431)),
+                np.reshape(zcr, (2, 431)),
+                np.reshape(sc, (2, 431)),
+                np.reshape(sr, (2, 431)),
+                np.reshape(sfm, (2, 431))
+            ],
+            axis=0
+        )
+
+        # Features mstd
+        fmstd = np.reshape(stats, (1, 10))
+
+        ##### Create datas #####
+        data = (
+            waveform,   # (2, 220500)
+            spectrogram,    # (2, 1025, 431)
+            features,   # (10, 431)
+            fmstd       # (1, 10)
+        )
+
+        #data = (waveform, spectrogram, rms, zcr, mel_spectrogram, stats)
         np.save(os.path.splitext(filename)[0] + ".npy", data)
         return data
 
 def getFilesInput(n):
     for i in range(n):
         getAllInputs(f"../Dataset/train/audio/{i}.wav")
-
-getFilesInput(1)
