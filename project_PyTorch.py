f0629a2d327b4eb4a0ea36b138cd6d61bcbd1db5
Valentin <valentin.vignal@gmail.com>
2019-04-14 00:11:01 +0800

    Change of the default learning rate 0.01 -> 0.001

--- project_PyTorch.py
+++ project_PyTorch.py
@@ -334,7 +334,7 @@ def main():
                         help='input batch size for testing (default: 16)')
     parser.add_argument('--epochs', type=int, default=50, metavar='N',
                         help='number of epochs to train (default: 50)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
                         help='learning rate (default: 0.001)')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
